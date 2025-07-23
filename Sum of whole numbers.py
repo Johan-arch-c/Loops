88b29ac60ff9bfe4dc9092c85d073773acfcc1f0
Johan-arch-c <johannithya@gmail.com>
2025-07-23 18:21:30 +1000

    hw

--- Sum of whole numbers.py
+++ Sum of whole numbers.py
@@ -1,4 +1,4 @@
-n=int(input("enter the whole number who's sum you wish to find"))
+n=int(input("enter the whole number that you want to find the sum of"))
 sum=0
 for i in range(1,n+1):
     sum=sum+i
